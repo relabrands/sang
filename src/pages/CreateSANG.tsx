6d8f1ce696aa5dc747391693744b191a3e167280
Robinson Sanchez <relabrands@gmail.com>
2026-01-12 22:48:50 -0400

    3 ccuenta bancaria

--- src/pages/CreateSANG.tsx
+++ src/pages/CreateSANG.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { ArrowLeft, DollarSign, Users, Calendar, Shuffle, ListOrdered, Check } from "lucide-react";
 import { Button } from "@/components/ui/button";
